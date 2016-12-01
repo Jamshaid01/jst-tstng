555da8754ae7dce7c6bbaae298fc450ed09f44aa
jamshaid <jamshaidali418@gmail.com>
2016-12-02 01:16:52 +0500

    more changes in codes

--- src/helloworldapp/Helloworldapp.java
+++ src/helloworldapp/Helloworldapp.java
@@ -16,6 +16,8 @@ public class Helloworldapp {
      */
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("second line");
+        
     }
     
 }
